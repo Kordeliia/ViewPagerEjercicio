4ec89ba196a9b81cf28bda68bc7e21aa614d1c4a
Kordeliia <lalvmun@hotmail.com>
2023-09-19 22:22:02 +0200

    Apartado tres. Adaptador para fragmentos.
    En este apartado, trabajamos con el adaptador de fragmentos.

--- app/src/main/java/com/example/viewpager/HomeFragment.kt
+++ app/src/main/java/com/example/viewpager/HomeFragment.kt
@@ -4,7 +4,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-class HomeFraagment : Fragment(){
+class HomeFragment : Fragment(){
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
--- app/src/main/java/com/example/viewpager/MainActivity.kt
+++ app/src/main/java/com/example/viewpager/MainActivity.kt
@@ -3,12 +3,21 @@ package com.example.viewpager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.viewpager.databinding.ActivityMainBinding
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var fragmentsAdapter: FragmentsAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        fragmentsAdapter = FragmentsAdapter(supportFragmentManager, lifecycle)
+        binding.viewPager.adapter = fragmentsAdapter
+        val titles = arrayOf("Inicio", "Carrito", "Favoritos")
+        TabLayoutMediator(binding.tabLayout, binding.viewPager){ tab, pos ->
+            tab.text = titles[pos]
+        }.attach()
     }
 }
